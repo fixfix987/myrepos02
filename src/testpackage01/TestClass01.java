abf4544191df9289c527c3e0f31c174cdc8087bd
My Name <you@example.com>
2021-10-02 02:39:50 +0900

    my commit001

---
+++ src/testpackage01/TestClass01.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package testpackage01;
+
+/**
+ * @author user1
+ *
+ */
+public class TestClass01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("hello world!");
+		
+
+	}
+
+}
